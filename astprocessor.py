c81581e6c462d069394e4cda8039ea26780b758d
somebody <ehern.lee@gmail.com>
2017-06-18 04:34:36 +0000

    Make AST show original

--- astprocessor.py
+++ astprocessor.py
@@ -7,17 +7,17 @@ def PassThrough(result):
 
 ASTProcessor = {
     CharcoalToken.Arrow: [
-        lambda result: "Left",
-        lambda result: "Up",
-        lambda result: "Right",
-        lambda result: "Down",
-        lambda result: "Up Left",
-        lambda result: "Up Right",
-        lambda result: "Down Right",
-        lambda result: "Down Left"
+        lambda result: result[0] + ": Left",
+        lambda result: result[0] + ": Up",
+        lambda result: result[0] + ": Right",
+        lambda result: result[0] + ": Down",
+        lambda result: result[0] + ": Up Left",
+        lambda result: result[0] + ": Up Right",
+        lambda result: result[0] + ": Down Right",
+        lambda result: result[0] + ": Down Left"
     ],
     CharcoalToken.Multidirectional: [
-        lambda result: ["Multidirectional"] + result[0][1:] + result[1][1:]
+        lambda result: [result[0] + ": Multidirectional"] + result[0][1:] + result[1][1:]
     ] + [
         lambda result: [result[1][0], result[0]] + result[1][1:]
     ] * (len(UnicodeGrammars[CharcoalToken.Multidirectional]) - 3) + [
@@ -28,13 +28,13 @@ ASTProcessor = {
         lambda result: ["Side"] + result
     ],
     CharcoalToken.String: [
-        lambda result: ["String \"%s\"" % result[0]]
+        lambda result: [repr(result[0]) + ": String %s" % repr(result[0])]
     ],
     CharcoalToken.Number: [
-        lambda result: ["Number %s" % str(result[0])]
+        lambda result: [str(result[0]) + ": Number %s" % str(result[0])]
     ],
     CharcoalToken.Name: [
-        lambda result: ["Identifier %s" % str(result[0])]
+        lambda result: [result[0] + ": Identifier %s" % str(result[0])]
     ],
     CharcoalToken.Separator: [
         lambda result: None,
@@ -47,14 +47,14 @@ ASTProcessor = {
             ["Stop", result[2]],
             ["Step", result[4]]
         ],
-        lambda result: ["Span", ["Start", result[0]], ["Step", result[3]]],
-        lambda result: ["Span", ["Start", result[0]], ["Stop", result[2]]],
-        lambda result: ["Span", ["Start", result[0]]],
-        lambda result: ["Span", ["Stop", result[1]], ["Step", result[3]]
+        lambda result: [result[0] + ": Span", ["Start", result[0]], ["Step", result[3]]],
+        lambda result: [result[0] + ": Span", ["Start", result[0]], ["Stop", result[2]]],
+        lambda result: [result[0] + ": Span", ["Start", result[0]]],
+        lambda result: [result[0] + ": Span", ["Stop", result[1]], ["Step", result[3]]
         ],
-        lambda result: ["Span", ["Stop", result[1]]],
-        lambda result: ["Span", ["Step", result[2]]],
-        lambda result: ["Span"]
+        lambda result: [result[0] + ": Span", ["Stop", result[1]]],
+        lambda result: [result[0] + ": Span", ["Step", result[2]]],
+        lambda result: [result[0] + ": Span"]
     ],
 
     CharcoalToken.Arrows: [
@@ -116,76 +116,76 @@ ASTProcessor = {
         lambda result: result[0]
     ],
     CharcoalToken.Nilary: [
-        lambda result: "Input string",
-        lambda result: "Input number",
-        lambda result: "Random",
-        lambda result: "Peek all",
-        lambda result: "Peek Moore",
-        lambda result: "Peek Von Neumann",
-        lambda result: "Peek",
+        lambda result: result[0] + ": Input string",
+        lambda result: result[0] + ": Input number",
+        lambda result: result[0] + ": Random",
+        lambda result: result[0] + ": Peek all",
+        lambda result: result[0] + ": Peek Moore",
+        lambda result: result[0] + ": Peek Von Neumann",
+        lambda result: result[0] + ": Peek",
     ],
     CharcoalToken.Unary: [
-        lambda result: "Negative",
-        lambda result: "Length",
-        lambda result: "Not",
-        lambda result: "Cast",
-        lambda result: "Random",
-        lambda result: "Evaluate",
+        lambda result: result[0] + ": Negative",
+        lambda result: result[0] + ": Length",
+        lambda result: result[0] + ": Not",
+        lambda result: result[0] + ": Cast",
+        lambda result: result[0] + ": Random",
+        lambda result: result[0] + ": Evaluate",
         lambda reuslt: "Pop",
-        lambda result: "To lowercase",
-        lambda result: "To uppercase",
-        lambda result: "Minimum",
-        lambda result: "Maximum",
-        lambda result: "Character/Ordinal",
-        lambda result: "Reverse",
-        lambda result: "Get variable",
-        lambda result: "Repeated",
-        lambda result: "Repeated null"
+        lambda result: result[0] + ": To lowercase",
+        lambda result: result[0] + ": To uppercase",
+        lambda result: result[0] + ": Minimum",
+        lambda result: result[0] + ": Maximum",
+        lambda result: result[0] + ": Character/Ordinal",
+        lambda result: result[0] + ": Reverse",
+        lambda result: result[0] + ": Get variable",
+        lambda result: result[0] + ": Repeated",
+        lambda result: result[0] + ": Repeated null"
     ],
     CharcoalToken.Binary: [
-        lambda result: "Sum",
-        lambda result: "Difference",
-        lambda result: "Product",
-        lambda result: "Quotient",
-        lambda result: "Integer quotient",
-        lambda result: "Modulo",
-        lambda result: "Equals",
-        lambda result: "Less than",
-        lambda result: "Greater than",
-        lambda result: "Bitwise and",
-        lambda result: "Bitwise or",
-        lambda result: "Inclusive range",
-        lambda result: "Mold",
-        lambda result: "Exponentiate",
-        lambda result: "At index",
-        lambda result: "Push",
-        lambda result: "Join",
-        lambda result: "Split",
-        lambda result: "Find all",
-        lambda result: "Find",
-        lambda result: "Pad left",
-        lambda result: "Pad right",
-        lambda result: "Count",
-        lambda result: "Rule",
-        lambda result: "Delayed rule",
-        lambda result: "Pattern test"
+        lambda result: result[0] + ": Sum",
+        lambda result: result[0] + ": Difference",
+        lambda result: result[0] + ": Product",
+        lambda result: result[0] + ": Quotient",
+        lambda result: result[0] + ": Integer quotient",
+        lambda result: result[0] + ": Modulo",
+        lambda result: result[0] + ": Equals",
+        lambda result: result[0] + ": Less than",
+        lambda result: result[0] + ": Greater than",
+        lambda result: result[0] + ": Bitwise and",
+        lambda result: result[0] + ": Bitwise or",
+        lambda result: result[0] + ": Inclusive range",
+        lambda result: result[0] + ": Mold",
+        lambda result: result[0] + ": Exponentiate",
+        lambda result: result[0] + ": At index",
+        lambda result: result[0] + ": Push",
+        lambda result: result[0] + ": Join",
+        lambda result: result[0] + ": Split",
+        lambda result: result[0] + ": Find all",
+        lambda result: result[0] + ": Find",
+        lambda result: result[0] + ": Pad left",
+        lambda result: result[0] + ": Pad right",
+        lambda result: result[0] + ": Count",
+        lambda result: result[0] + ": Rule",
+        lambda result: result[0] + ": Delayed rule",
+        lambda result: result[0] + ": Pattern test"
     ],
     CharcoalToken.Ternary: [
     ],
     CharcoalToken.LazyUnary: [
     ],
     CharcoalToken.LazyBinary: [
-        lambda result: "And",
-        lambda result: "Or"
+        lambda result: result[0] + ": And",
+        lambda result: result[0] + ": Or"
     ],
     CharcoalToken.LazyTernary: [
-        lambda result: "Ternary"
+        lambda result: result[0] + ": Ternary"
     ],
     CharcoalToken.OtherOperator: [
-        lambda result: ["Peek direction"] + result[1:],
-        lambda result: ["Map"] + result[1:],
-        lambda result: ["Evaluate variable"] + result[1:],
-        lambda result: ["Evaluate variable"] + result[1:]
+        lambda result: [result[0] + ": Peek direction"] + result[1:],
+        lambda result: [result[0] + ": Map"] + result[1:],
+        lambda result: [result[0] + ": Evaluate variable"] + result[1:],
+        lambda result: [result[0] + ": Evaluate variable"] + result[1:]
     ],
 
     CharcoalToken.Program: [
@@ -197,79 +197,79 @@ ASTProcessor = {
         lambda result: result[0]
     ],
     CharcoalToken.Command: [
-        lambda result: ["Input String", result[1]],
-        lambda result: ["Input Number", result[1]],
-        lambda result: ["Evaluate", result[1]],
+        lambda result: [result[0] + ": Input String", result[1]],
+        lambda result: [result[0] + ": Input Number", result[1]],
+        lambda result: [result[0] + ": Evaluate", result[1]],
         lambda result: ["Print"] + result,
-        lambda result: ["Print"] + result,
-        lambda result: ["Multiprint"] + result[1:],
-        lambda result: ["Multiprint"] + result[1:],
-        lambda result: ["Polygon"] + result[1:],
-        lambda result: ["Polygon"] + result[1:],
-        lambda result: ["Hollow Polygon"] + result[1:],
-        lambda result: ["Hollow Polygon"] + result[1:],
-        lambda result: ["Rectangle"] + result[1:],
-        lambda result: ["Rectangle"] + result[1:],
-        lambda result: ["Oblong"] + result[1:],
-        lambda result: ["Oblong"] + result[1:],
-        lambda result: ["Box"] + result[1:],
-        lambda result: ["Box"] + result[1:],
+        lambda result: [result[0] + ": Print"] + result,
+        lambda result: [result[0] + ": Multiprint"] + result[1:],
+        lambda result: [result[0] + ": Multiprint"] + result[1:],
+        lambda result: [result[0] + ": Polygon"] + result[1:],
+        lambda result: [result[0] + ": Polygon"] + result[1:],
+        lambda result: [result[0] + ": Hollow Polygon"] + result[1:],
+        lambda result: [result[0] + ": Hollow Polygon"] + result[1:],
+        lambda result: [result[0] + ": Rectangle"] + result[1:],
+        lambda result: [result[0] + ": Rectangle"] + result[1:],
+        lambda result: [result[0] + ": Oblong"] + result[1:],
+        lambda result: [result[0] + ": Oblong"] + result[1:],
+        lambda result: [result[0] + ": Box"] + result[1:],
+        lambda result: [result[0] + ": Box"] + result[1:],
         lambda result: ["Move"] + result,
-        lambda result: ["Move"] + result[1:],
-        lambda result: ["Move"] + result[1:],
-        lambda result: ["Jump"] + result[1:],
-        lambda result: ["Pivot Left", result[1]],
-        lambda result: ["Pivot Left"],
-        lambda result: ["Pivot Right", result[1]],
-        lambda result: ["Pivot Right"],
-        lambda result: ["Jump to"] + result[1:],
-        lambda result: ["Rotate transform"] + result[1:],
-        lambda result: ["Rotate transform"]
+        lambda result: [result[0] + ": Move"] + result[1:],
+        lambda result: [result[0] + ": Move"] + result[1:],
+        lambda result: [result[0] + ": Jump"] + result[1:],
+        lambda result: [result[0] + ": Pivot Left", result[1]],
+        lambda result: [result[0] + ": Pivot Left"],
+        lambda result: [result[0] + ": Pivot Right", result[1]],
+        lambda result: [result[0] + ": Pivot Right"],
+        lambda result: [result[0] + ": Jump to"] + result[1:],
+        lambda result: [result[0] + ": Rotate transform"] + result[1:],
+        lambda result: [result[0] + ": Rotate transform"]
     ] +
-    [lambda result: ["Reflect transform"] + result[1:]] * 3 +
-    [lambda result: ["Rotate prism"] + result[1:]] * 6 +
-    [lambda result: ["Reflect mirror"] + result[1:]] * 3 +
-    [lambda result: ["Rotate copy"] + result[1:]] * 6 +
-    [lambda result: ["Reflect copy"] + result[1:]] * 3 +
-    [lambda result: ["Rotate overlap overlap"] + result[1:]] * 6 +
-    [lambda result: ["Rotate overlap"] + result[1:]] * 6 +
-    [lambda result: ["Rotate shutter overlap"] + result[1:]] * 6 +
-    [lambda result: ["Rotate shutter"] + result[1:]] * 6 +
-    [lambda result: ["Reflect overlap overlap"] + result[1:]] * 3 +
-    [lambda result: ["Reflect overlap"] + result[1:]] * 3 +
-    [lambda result: ["Reflect butterfly overlap"] + result[1:]] * 3 +
-    [lambda result: ["Reflect butterfly"] + result[1:]] * 3 +
+    [lambda result: [result[0] + ": Reflect transform"] + result[1:]] * 3 +
+    [lambda result: [result[0] + ": Rotate prism"] + result[1:]] * 6 +
+    [lambda result: [result[0] + ": Reflect mirror"] + result[1:]] * 3 +
+    [lambda result: [result[0] + ": Rotate copy"] + result[1:]] * 6 +
+    [lambda result: [result[0] + ": Reflect copy"] + result[1:]] * 3 +
+    [lambda result: [result[0] + ": Rotate overlap overlap"] + result[1:]] * 6 +
+    [lambda result: [result[0] + ": Rotate overlap"] + result[1:]] * 6 +
+    [lambda result: [result[0] + ": Rotate shutter overlap"] + result[1:]] * 6 +
+    [lambda result: [result[0] + ": Rotate shutter"] + result[1:]] * 6 +
+    [lambda result: [result[0] + ": Reflect overlap overlap"] + result[1:]] * 3 +
+    [lambda result: [result[0] + ": Reflect overlap"] + result[1:]] * 3 +
+    [lambda result: [result[0] + ": Reflect butterfly overlap"] + result[1:]] * 3 +
+    [lambda result: [result[0] + ": Reflect butterfly"] + result[1:]] * 3 +
     [
-        lambda result: ["Rotate"] + result[1:],
-        lambda result: ["Rotate"],
-        lambda result: ["Reflect"] + result[1:],
-        lambda result: ["Reflect"],
-        lambda result: ["Copy"] + result[1:],
-        lambda result: ["For"] + result[1:],
-        lambda result: ["While"] + result[1:],
-        lambda result: ["If"] + result[1:],
-        lambda result: ["If"] + result[1:],
-        lambda result: ["Assign at index"] + result[1:],
-        lambda result: ["Assign"] + result[1:],
-        lambda result: ["Assign"] + result[1:],
-        lambda result: ["Fill"] + result[1:],
-        lambda result: ["SetBackground", result[1]],
-        lambda result: ["Dump"],
-        lambda result: ["Refresh for"] + result[1:],
-        lambda result: ["Refresh while"] + result[1:],
-        lambda result: ["Refresh", result[1]],
-        lambda result: ["Refresh"],
-        lambda result: ["Toggle trim"],
-        lambda result: ["Crop", result[1], result[2]],
-        lambda result: ["Crop", result[1]],
-        lambda result: ["Clear"],
-        lambda result: ["Extend", result[1], result[2]],
-        lambda result: ["Extend", result[1]],
-        lambda result: ["Push"] + result[1:],
-        lambda result: ["Switch"] + result[1:],
-        lambda result: ["Map"] + result[1:],
-        lambda result: ["Execute variable"] + result[1:],
-        lambda result: ["Execute variable"] + result[1:],
-        lambda result: ["Set variable"] + result[1:]
+        lambda result: [result[0] + ": Rotate"] + result[1:],
+        lambda result: [result[0] + ": Rotate"],
+        lambda result: [result[0] + ": Reflect"] + result[1:],
+        lambda result: [result[0] + ": Reflect"],
+        lambda result: [result[0] + ": Copy"] + result[1:],
+        lambda result: [result[0] + ": For"] + result[1:],
+        lambda result: [result[0] + ": While"] + result[1:],
+        lambda result: [result[0] + ": If"] + result[1:],
+        lambda result: [result[0] + ": If"] + result[1:],
+        lambda result: [result[0] + ": Assign at index"] + result[1:],
+        lambda result: [result[0] + ": Assign"] + result[1:],
+        lambda result: [result[0] + ": Assign"] + result[1:],
+        lambda result: [result[0] + ": Fill"] + result[1:],
+        lambda result: [result[0] + ": SetBackground", result[1]],
+        lambda result: [result[0] + ": Dump"],
+        lambda result: [result[0] + ": Refresh for"] + result[1:],
+        lambda result: [result[0] + ": Refresh while"] + result[1:],
+        lambda result: [result[0] + ": Refresh", result[1]],
+        lambda result: [result[0] + ": Refresh"],
+        lambda result: [result[0] + ": Toggle trim"],
+        lambda result: [result[0] + ": Crop", result[1], result[2]],
+        lambda result: [result[0] + ": Crop", result[1]],
+        lambda result: [result[0] + ": Clear"],
+        lambda result: [result[0] + ": Extend", result[1], result[2]],
+        lambda result: [result[0] + ": Extend", result[1]],
+        lambda result: [result[0] + ": Push"] + result[1:],
+        lambda result: [result[0] + ": Switch"] + result[1:],
+        lambda result: [result[0] + ": Map"] + result[1:],
+        lambda result: [result[0] + ": Execute variable"] + result[1:],
+        lambda result: [result[0] + ": Execute variable"] + result[1:],
+        lambda result: [result[0] + ": Set variable"] + result[1:]
     ]
 }
